15eafb21b2ff52e99052e23141327da2667fffc3
xingqian <songflower1@126.com>
2018-01-05 14:45:41 +0800

    修改提交github

--- src/main/java/SpringController.java
+++ src/main/java/SpringController.java
@@ -2,6 +2,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// 修改
 public class SpringController {
 
 	public static void main(String[] args) {

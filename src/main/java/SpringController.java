fbd1ea1ecfbf6a2b32d4355960bc473387bfd0cc
xingqian <songflower1@126.com>
2018-01-05 14:35:45 +0800

    提交demo-test

---
+++ src/main/java/SpringController.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringController {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringController.class, args);
+	}
+
+}

b8f5b0e64dd2e1f77e85b6601281ed88506ea082
xingqian <songflower1@126.com>
2018-04-08 17:49:29 +0800

    uu

--- src/main/java/SpringController.java
+++ src/main/java/SpringController.java
@@ -14,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 // 修改2
+// ddd
 public class SpringController {
 
 	public static void main(String[] args) {

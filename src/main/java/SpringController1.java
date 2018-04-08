67bc05976d433374f7f3fc85ccd748fe481eca4d
xingqian <songflower1@126.com>
2018-04-08 17:47:19 +0800

    k

--- src/main/java/SpringController1.java
+++ src/main/java/SpringController1.java
@@ -13,7 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Copyright (c) 2010-2011 .All rights reserved.<br/>
  */
 @SpringBootApplication
-// 修改2eee
+// 修改2eee99
 public class SpringController1 {
 
 	public static void main(String[] args) {

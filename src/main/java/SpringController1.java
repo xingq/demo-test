7eb23b4d5968e6adad4f635969c33a9177dbfa38
xingqian <songflower1@126.com>
2018-04-08 17:20:32 +0800

    mm

---
+++ src/main/java/SpringController1.java
@@ -0,0 +1,24 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 文件名称： SpringController.java</br>
+ * 初始作者： xingqian</br>
+ * 创建日期： 2018年1月5日</br>
+ * 功能说明： 这里用一句话描述这个类的作用--此句话需删除 <br/>
+ * =================================================<br/>
+ * 修改记录：<br/>
+ * 修改作者 日期 修改内容<br/>
+ * ================================================<br/>
+ * Copyright (c) 2010-2011 .All rights reserved.<br/>
+ */
+@SpringBootApplication
+// 修改2
+public class SpringController {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringController.class, args);
+	}
+
+}
